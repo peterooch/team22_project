fc2dfee550373ac27e5ef99a8ee7b8b4ddceaddb
Oren Shaya <xxorenxx@gmail.com>
2021-04-04 13:06:21 +0300

    BSPM2021T22-44

--- student_board/FacultyRegister/models.py
+++ student_board/FacultyRegister/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+<<<<<<< Updated upstream
 # Create your models here.
 
+=======
+#Name, ID, Courses, Email, Password, Approved
+>>>>>>> Stashed changes
 class User(models.Model):
     first_name =  models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
@@ -9,6 +13,13 @@ class User(models.Model):
     email = models.EmailField()
     password = models.TextField()
 
+<<<<<<< Updated upstream
 class Faculty(User):
     courses = models.CharField(max_length=50)
     approved = models.BooleanField(default=False)
+=======
+
+class Faculty(User):
+    courses = models.CharField(max_length=50)
+    approved = models.BooleanField(default=False)
+>>>>>>> Stashed changes
--- student_board/FacultyRegister/views.py
+++ student_board/FacultyRegister/views.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
@@ -10,3 +11,24 @@ def FacultyRegister(request):
     context= {}
     #return render(request,'FacultyRegister/FacultyRegister', context)
     return HttpResponse('Faculty Register')
+=======
+from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseRedirect
+from .models import Faculty
+
+def index(request):
+    return render(request, 'register.html')
+
+def adduser(request):
+    details = {
+        'first_name' : request.POST['firstname'],
+        'last_name'  : request.POST['lastname'],
+        'Id'         : request.POST['ID'],
+        'email'      : request.POST['email'],
+        'password'   : request.POST['psw'],
+        'courses'    : request.POST['courses']
+    }
+    faculty = Faculty(**details)
+    faculty.save()
+    return HttpResponseRedirect('/')
+>>>>>>> Stashed changes
