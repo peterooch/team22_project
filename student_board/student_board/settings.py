0b84fa36a97b3e1abeeb6cb0da27d3c936acbcc9
roiamz <roiamzallag@gmail.com>
2021-04-04 13:50:04 +0300

    Initial admin system

--- student_board/student_board/settings.py
+++ student_board/student_board/settings.py
@@ -33,8 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'register.apps.RegisterConfig',
     'posts.apps.PostsConfig',
-
-    'django.contrib.admin',
+    'admin.apps.AdminConfig',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
