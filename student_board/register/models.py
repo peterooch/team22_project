d4ae378e9a22cdd950fd1e16cc6b7c70b3b08eaa
Baruch Rutman <peterooch@gmail.com>
2021-04-04 15:58:32 +0300

    Add Jenkins pipeline
    Use sqlite3 for tests instead of postgresql

--- student_board/register/models.py
+++ student_board/register/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class User(models.Model):
+    id = models.CharField(primary_key=True, max_length=10)
     first_name =  models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    Id = models.CharField(max_length=10)
     email = models.EmailField()
     password = models.TextField()
 
