4d9fd0ce23aabccc439ba050668cf9b26c346901
roiamz <roiamzallag@gmail.com>
2021-04-07 17:29:32 +0300

    backend and  fronted for admin login

--- student_board/login/views.py
+++ student_board/login/views.py
@@ -1,3 +1,4 @@
+from student_board.register.models import Admin
 from django.shortcuts import render
 from django.contrib import messages
 from django.http import HttpResponse
@@ -5,27 +6,26 @@ from django.contrib.auth import authenticate, login, logout
 from django.apps import apps
 
 
-
-from register.models import User, Student, Faculty, Admin
 # Create your views here.
 
 def login(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
-
+        model_names = ["Admin", "Faculty", "Student"]
         user = authenticate(request, username=None, password=password, email=email)
         
         if user is not None:
             #login(request, user)
             request.session['user'] = user.id
 
-            # check if the user is a student
-            StudentModel = apps.get_model('register', 'Student')
-            if StudentModel.objects.filter(id=user.id).exists():
-                return HttpResponse("student logged in") #FIXME: redirect to student homepage
+            for model in model_names:
+                Model = apps.get_model('register', model)
+                if Model.object.filter(id=user.id).exists():
+                    request.session['user_type'] = model
+                    return HttpResponse('/')
 
-            #FIXME: check if the user is faculty or admin
+            
         else:
             messages.info(request,'Email or password is incorrect')
     context={}
