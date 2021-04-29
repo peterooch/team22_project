8c08faae6d7ae22f4202a2237712062d5424e3fb
Oren Shaya <xxorenxx@gmail.com>
2021-04-29 19:06:13 +0300

    added testing

--- student_board/posts/tests.py
+++ student_board/posts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Post
+from django.utils import timezone
 
-# Create your tests here.
+class postTestCase(TestCase):
+    def setUp(self):
+        Post.objects.create(
+            poster    = 'me',
+            date      = timezone.now(),
+            title     = 'test',
+            content   = 'checking for test',
+            forum_id  = 'milga'
+        )
+
+    def test_postDB_check(self):
+        post = Post.objects.get(poster = 'me')
+        self.assertEqual(post.title, 'test')
+            
