227a058aae2462ff4b22f7ca4ac7406460f9efcc
rotem vertman <rotem.ver@gmail.com>
2021-05-05 18:25:47 +0300

    summaries unit test

--- student_board/summaries/tests.py
+++ student_board/summaries/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Documents
+from student_board import settings
+
 
 # Create your tests here.
+class docTest(TestCase):
+    def setUp(self):
+        Documents.objects.create(location=settings.MEDIA_ROOT, user="123456789", title='doc', course='db', file_size=0)
+
+    def test_doc(self):
+        doc = Documents.objects.get(title="doc")
+        self.assertEqual(doc.user, '123456789')
+        
