f4ae959465f3c678681d2d2e2ecc0e3b87f3ccba
Oren Shaya <xxorenxx@gmail.com>
2021-06-03 21:53:50 +0300

    Added quiz test

--- student_board/quiz/tests.py
+++ student_board/quiz/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from quiz.models import Question,Choice
+from register.models import User
+from django.utils import timezone
 
-# Create your tests here.
+class quiz_test(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id='123',
+            first_name='dummy',
+            last_name='dummy',
+            email='dummy@mail.com',
+            password='dummy'
+        )
+        user = User.objects.get(id='123')
+        Question.objects.create(
+            question_text='question dummy',
+            pub_date=timezone.now(), 
+            poster=user
+        )
+        question = Question.objects.get(question_text='question dummy')
+        Choice.objects.create(
+            question=question,
+            choice_text='right answer',
+            correct=True)
+        Choice.objects.create(
+            question=question,
+            choice_text='wrong answer',
+            correct=False)
+        Choice.objects.create(
+            question=question,
+            choice_text='wrong2 answer',
+            correct=False)
+        Choice.objects.create(
+            question=question,
+            choice_text='wrong3 answer',
+            correct=False)
+
+    def test_quiz(self):
+        question = Question.objects.get(question_text='question dummy')
+        c = question.choice_set.get(choice_text='right answer')
+        self.assertEqual(c.correct, True)
+
+        post_req = self.rf.post('/quiz/submit_quiz', {'question_text': question.question_text,
+        'correct1': c, 'text1': c.choice_text})
+        post_req.session = {'user': '1234'}
+        views.submit_quiz(post_req)
+        q = Question.objects.last()
+        self.assertEqual(q.poster.id, '123')      
