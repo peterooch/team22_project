c528f43de37af4b3afdb5658b28053c5d1e19ff1
Oren Shaya <xxorenxx@gmail.com>
2021-06-03 22:31:33 +0300

    Fixed Quiz test

--- student_board/quiz/tests.py
+++ student_board/quiz/tests.py
@@ -15,38 +15,38 @@ class quiz_test(TestCase):
             email='dummy@mail.com',
             password='dummy'
         )
-        user = User.objects.get(id='123')
+        self.user = User.objects.get(id='123')
         Question.objects.create(
             question_text='question dummy',
             pub_date=timezone.now(), 
-            poster=user
+            poster=self.user
         )
-        question = Question.objects.get(question_text='question dummy')
+        self.question = Question.objects.get(question_text='question dummy')
         Choice.objects.create(
-            question=question,
+            question=self.question,
             choice_text='right answer',
             correct=True)
         Choice.objects.create(
-            question=question,
+            question=self.question,
             choice_text='wrong answer',
             correct=False)
         Choice.objects.create(
-            question=question,
+            question=self.question,
             choice_text='wrong2 answer',
             correct=False)
         Choice.objects.create(
-            question=question,
+            question=self.question,
             choice_text='wrong3 answer',
             correct=False)
 
     def test_quiz(self):
-        question = Question.objects.get(question_text='question dummy')
-        c = question.choice_set.get(choice_text='right answer')
-        self.assertEqual(c.correct, True)
-
-        post_req = self.rf.post('/quiz/submit_quiz', {'question_text': question.question_text,
-        'correct1': c, 'text1': c.choice_text})
-        post_req.session = {'user': '1234'}
+        self.c = self.question.choice_set.get(choice_text='right answer')
+        self.assertEqual(self.c.correct, True)
+        self.c2 = self.question.choice_set.get(choice_text='wrong answer')
+        post_req = self.rf.post('/quiz/submit_quiz', {'question_text': self.question.question_text,
+        'choice_text1': self.c.choice_text,'choice_text2': self.c2.choice_text,
+        'choice_text3': self.c2.choice_text,'choice_text4': self.c2.choice_text})
+        post_req.session = {'user': '123'}
         views.submit_quiz(post_req)
         q = Question.objects.last()
-        self.assertEqual(q.poster.id, '123')      
+        self.assertEqual(q.poster, 'dummy')      
